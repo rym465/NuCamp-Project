e2d271377367d444d253e706176a836466ed467b
Renee Matthews <reneeymatthews@gmail.com>
2021-05-28 23:26:13 -0400

    Project Additions and Edits - New Pages

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+//  RESERVE BUTTON-->
+$("#reserveButton").click(function() {
+    $("#reserveModal").modal("show" );
+    });
+
+//  LOGIN BUTTON-->
+$("#loginButton").click(function() {
+    $("#loginModal").modal("show" );
+    });
